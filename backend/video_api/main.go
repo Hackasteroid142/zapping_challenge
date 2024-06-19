6a0fdf8f3d7a7bf2ece5543bf4eabb09061d7310
Vicente Vega <vicente.vega@usach.cl>
2024-06-19 09:49:52 -0400

    feat: docker y estructura inicial para endpoint de streaming

---
+++ backend/video_api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("files"))
+	http.Handle("/", fs)
+
+	fmt.Println("Listening on :3000")
+	err := http.ListenAndServe(":3000", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
