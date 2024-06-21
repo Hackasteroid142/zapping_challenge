d6d5c1125d62231cf19ea5662c16f6fbf4df7b27
Vicente Vega <vicente.vega@usach.cl>
2024-06-20 22:18:52 -0400

    feat: visualización de stream

---
+++ streaming_frontend/src/pages/live.vue
@@ -0,0 +1,28 @@
+<template>
+  <AppBar />
+  <v-container class="d-flex align-center justify-center video-container" fluid>
+    <video-player :options="videoOptions" class="video-player" />
+  </v-container>
+</template>
+
+<script>
+import "video.js/dist/video-js.css";
+
+export default {
+  data() {
+    return {
+      videoOptions: {
+        autoplay: true,
+        controls: true,
+        muted: true,
+        sources: [
+          {
+            src: "http://localhost:3001/segment.m3u8",
+            type: "application/x-mpegURL",
+          },
+        ],
+      },
+    };
+  },
+};
+</script>
